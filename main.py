67a9a3d3cad048fcd2416e92ccfe9ea52b7e4d98
Sahil Tomar <148889601+dev-S-t@users.noreply.github.com>
2024-06-11 12:06:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -143,6 +143,3 @@ def predict(transaction: Transaction):
         raise HTTPException(status_code=500, detail="An unexpected error occurred.")
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
